4575d001c90feb3cbe4a87892ee65f21400d7e85
Marcio Santos <mvs_designer@hotmail.com>
2018-12-05 11:28:30 -0200

    sale module OK

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -4,42 +4,48 @@ class SalesController < ApplicationController
   # GET /sales
   # GET /sales.json
   def index
-    @sales = Sale.all
+    @sales = Question.where('group_id = 9')
+    @answer = Answer.where('user_id = ?', current_user.id)
   end
 
   # GET /sales/1
   # GET /sales/1.json
   def show
+    redirect_to answers_path
   end
 
   # GET /sales/new
   def new
-    @sale = Sale.new
+    @answer = Answer.new
+    @questions = Question.where('group_id = 9')
+    @method = 'create'
   end
 
   # GET /sales/1/edit
   def edit
+    redirect_to answers_path
   end
 
   # POST /sales
   # POST /sales.json
   def create
-    @sale = Sale.new(sale_params)
-
+    params[:sale][:question].each do |k, v|
+      answer = Answer.new
+      answer.user_id = params[:sale][:user_id]
+      answer.question_id = k;
+      answer.answer = v;
+      answer.save
+    end
     respond_to do |format|
-      if @sale.save
-        format.html { redirect_to @sale, notice: 'Sale was successfully created.' }
-        format.json { render :show, status: :created, location: @sale }
-      else
-        format.html { render :new }
-        format.json { render json: @sale.errors, status: :unprocessable_entity }
-      end
+      format.html { redirect_to answers_path, notice: 'Questionário respondido com sucesso.' }
+      format.json { render :index, status: :created, location: @answer }
     end
   end
 
   # PATCH/PUT /sales/1
   # PATCH/PUT /sales/1.json
   def update
+    redirect_to answers_path
     respond_to do |format|
       if @sale.update(sale_params)
         format.html { redirect_to @sale, notice: 'Sale was successfully updated.' }
@@ -54,6 +60,7 @@ class SalesController < ApplicationController
   # DELETE /sales/1
   # DELETE /sales/1.json
   def destroy
+    redirect_to answers_path
     @sale.destroy
     respond_to do |format|
       format.html { redirect_to sales_url, notice: 'Sale was successfully destroyed.' }
@@ -64,11 +71,13 @@ class SalesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_sale
-      @sale = Sale.find(params[:id])
+      redirect_to answers_path
+      # @sale = Sale.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def sale_params
+      redirect_to answers_path
       params.fetch(:sale, {})
     end
 end
